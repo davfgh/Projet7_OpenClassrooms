636ee8539db05c9db3849077a46f38795aaf1789
davfgh <98954207+davfgh@users.noreply.github.com>
2025-02-18 19:56:14 +0100

    deployment

--- startup.sh
+++ startup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd /home/site/wwwroot
 source .venv/bin/activate
-# gunicorn -w 2 api.app:app --bind 0.0.0.0:8000
-export PORT=5000  # Ajout explicite du port
+export PORT=8000  # Ajout explicite du port
 gunicorn -w 2 --chdir api app:app --bind 0.0.0.0:$PORT
