a9c23ae045d64e6adb4af5fb596d033029c7b1cb
davfgh <98954207+davfgh@users.noreply.github.com>
2025-02-18 07:39:32 +0100

    Optimisation du déploiement

--- api/startup.sh
+++ api/startup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cd /home/site/wwwroot
 source .venv/bin/activate
-PORT=${PORT:-8000}
-gunicorn -w 2 -k uvicorn.workers.UvicornWorker app:app --bind 0.0.0.0:8000
+gunicorn -w 2 -k uvicorn.workers.UvicornWorker api.app:app --bind 0.0.0.0:8000
