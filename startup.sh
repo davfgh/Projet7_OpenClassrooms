8e901a68c42ae0fcba26f55abf1c28cb533a42a1
davfgh <98954207+davfgh@users.noreply.github.com>
2025-02-18 18:11:37 +0100

    Déploiement

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/site/wwwroot
 source .venv/bin/activate
-gunicorn -w 2 -k uvicorn.workers.UvicornWorker api.app:app --bind 0.0.0.0:8000
+gunicorn -w 2 api.app:app --bind 0.0.0.0:8000
